9b048dfcc094fa525da8636df84c8b312cab9455
kshelar0 <kshelar0@gmail.com>
2021-11-05 20:38:42 +0530

    Second commit after few chnages in Test1

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/45/40d46cdb493e001c1cdecdb4a4dc2d2c
@@ -0,0 +1,22 @@
+package LearnMaven.L_Maven1;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void method1Test()
+	{
+		System.out.println("Method 1 called");
+	}
+	@Test
+	public void method2()
+	{
+		System.out.println("Method 2 called");
+	}
+	@Test
+	public void method3()
+	{
+		System.out.println("Method 3 called");
+	}
+
+}
---
+++ L_Maven1/src/test/java/LearnMaven/L_Maven1/Test1.java
@@ -0,0 +1,23 @@
+package LearnMaven.L_Maven1;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+	@Test
+	public void method1Test()
+	{
+		System.out.println("Method 1 called");
+		System.out.println("Added new Method");
+	}
+	@Test
+	public void method2()
+	{
+		System.out.println("Method 2 called");
+	}
+	@Test
+	public void method3()
+	{
+		System.out.println("Method 3 called");
+	}
+
+}
